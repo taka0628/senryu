9e8c88ae570f664a25e55ee4db6521879dda7c69
kawayama <kawa130307@gmail.com>
2023-09-16 22:12:22 +0900

    corsの設定変更

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_socketio import SocketIO
 
 from api import results, topics
@@ -6,6 +7,7 @@ from api import results, topics
 app = Flask(__name__)
 app.register_blueprint(results.app)
 app.register_blueprint(topics.app)
+CORS(app, origins="*")
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 if __name__ == "__main__":
