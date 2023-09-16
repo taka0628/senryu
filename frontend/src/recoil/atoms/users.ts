21eed708aa2cb834742b2e079efe3aef2f9177e8
SNKK62 <koki.seno62@gmail.com>
2023-09-16 18:42:30 +0900

    川柳投稿ページロジック

--- frontend/src/recoil/atoms/users.ts
+++ frontend/src/recoil/atoms/users.ts
@@ -1,6 +1,8 @@
 import { atom } from 'recoil';
 interface User {
   name: string;
+  topic?: string | undefined;
+  senryu?: string | undefined;
 }
 export const usersAtom = atom<User[]>({
   key: 'users',
