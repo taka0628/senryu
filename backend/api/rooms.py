ccb18024bf8f75369ba19e6277fe346adce26e64
kawayama <kawa130307@gmail.com>
2023-09-17 10:49:12 +0900

    ルーム参加時の処理を追加

--- backend/api/rooms.py
+++ backend/api/rooms.py
@@ -1,7 +1,9 @@
 import random
+import uuid
 
 from flask import Blueprint, jsonify, request
 
+from main import socketio
 from models import room_form
 from db import db
 
@@ -65,8 +67,44 @@ def create_room():
         'theme': theme,
         'wolf_topic': wolf_topic,
         'non_wolf_topic': non_wolf_topic,
+        'wolf_player_num': random.randint(0, data['user_cnt'] - 1),
+        'player_list': [],
     }
 
-    print(f"Created room {room_id} ({rooms})")
+    print(f"Created room {room_id} ({rooms[room_id]})")
 
-    return jsonify({"status": "success"})
+    return jsonify(rooms[room_id])
+
+
+@socketio.on('join_room')
+def join_room(data):
+    room_id = data['room_id']
+    username = data['name']
+    # TODO: session_idをプレイや情報にいれて、送信時もそれを使う
+    session_id = request.sid # type: ignore
+
+    if room_id not in rooms:
+        print(f"Error: room {room_id} not found ({rooms})")
+        socketio.emit('error', {"status": "error", "errors": "room not found"})
+        return
+    elif len(rooms[room_id]['player_list']) >= rooms[room_id]['user_cnt']:
+        print(f"Error: room {room_id} is full ({rooms})")
+        socketio.emit('error', {"status": "error", "errors": "room is full"})
+        return
+    
+    # NOTE: 衝突する可能性あり
+    is_wolf = len(rooms[room_id]['player_list']) == rooms[room_id]['wolf_player_num']
+
+    rooms[room_id]['player_list'].append({
+        'id': str(uuid.uuid4()),
+        'name': username,
+        'topic': rooms[room_id]['wolf_topic'] if is_wolf else rooms[room_id]['non_wolf_topic'],
+        'senryu': None, # 川柳を受け取ったら値が入る
+        'is_wolf': is_wolf,
+    })
+
+    if len(rooms[room_id]['player_list']) == rooms[room_id]['user_cnt']:
+        # 全員揃ったらゲーム開始
+        print(f"Game started in room {room_id} ({rooms[room_id]})")
+        socketio.emit('gather_member', )
+        return
